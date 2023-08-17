b77215c4fa96b59ad221378e17043ba97ca6da7d
demelcjohn <demal.john007@gmail.com>
2023-08-17 21:23:44 +0530

    heap sort

---
+++ Cycle-6/HeapSort.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class HeapSort{
+
+Scanner input = new Scanner(System.in);
+public static void main(String args[]){
+int arr[] = int[50];
+System.out.println("Heap Sort");
+
+}
+}
